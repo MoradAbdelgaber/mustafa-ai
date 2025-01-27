edceccd1bf7b74571b579c513d092c56be15b254
Morad Abdelgaber <moradabdelgaber1@gmail.com>
2025-01-27 15:09:41 +0200

    enhance

--- config/db.js
+++ config/db.js
@@ -1,16 +1,18 @@
 // config/db.js
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
+      useCreateIndex: true,
       useNewUrlParser: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
+      useFindAndModify: false,
     });
-    console.log('MongoDB connected successfully.');
+    console.log("MongoDB connected successfully.");
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
+    console.error("Error connecting to MongoDB:", error);
     process.exit(1);
   }
 };
