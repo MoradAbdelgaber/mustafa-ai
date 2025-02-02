54ea01e9fd219b40f34f5f4a65c607a4e7138d21
Morad Abdelgaber <moradabdelgaber1@gmail.com>
2025-02-02 11:05:37 +0200

    multi employee update

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -1,13 +1,18 @@
 // routes/employeeRoutes.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { authMiddleware } = require('../auth');
-const employeeController = require('../controllers/employeeController');
+const { authMiddleware } = require("../auth");
+const employeeController = require("../controllers/employeeController");
 
-router.post('/', authMiddleware, employeeController.createEmployee);
-router.get('/', authMiddleware, employeeController.getEmployees);
-router.get('/:id', authMiddleware, employeeController.getEmployeeById);
-router.put('/:id', authMiddleware, employeeController.updateEmployee);
-router.delete('/:id', authMiddleware, employeeController.deleteEmployee);
+router.post("/", authMiddleware, employeeController.createEmployee);
+router.get("/", authMiddleware, employeeController.getEmployees);
+router.get("/:id", authMiddleware, employeeController.getEmployeeById);
+router.put(
+  "/multi-update",
+  authMiddleware,
+  employeeController.multiUpdateEmployees
+);
+router.put("/:id", authMiddleware, employeeController.updateEmployee);
+router.delete("/:id", authMiddleware, employeeController.deleteEmployee);
 
 module.exports = router;
