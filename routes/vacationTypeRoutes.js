e931823cf0f18465d1c03fe99e62f010a028d393
Morad Abdelgaber <moradabdelgaber1@gmail.com>
2025-02-11 17:09:05 +0200

    enahnce

--- routes/vacationTypeRoutes.js
+++ routes/vacationTypeRoutes.js
@@ -1,13 +1,14 @@
 // routes/vacationTypeRoutes.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { authMiddleware } = require('../auth');
-const ctrl = require('../controllers/vacationTypeController');
+const { authMiddleware, authEmployeeMiddleware } = require("../auth");
+const ctrl = require("../controllers/vacationTypeController");
 
-router.post('/', authMiddleware, ctrl.createVacationType);
-router.get('/', authMiddleware, ctrl.getAllVacationTypes);
-router.get('/:id', authMiddleware, ctrl.getVacationTypeById);
-router.put('/:id', authMiddleware, ctrl.updateVacationType);
-router.delete('/:id', authMiddleware, ctrl.deleteVacationType);
+router.post("/", authMiddleware, ctrl.createVacationType);
+router.get("/", authMiddleware, ctrl.getAllVacationTypes);
+router.get("/employee", authEmployeeMiddleware, ctrl.getAllVacationTypes);
+router.get("/:id", authMiddleware, ctrl.getVacationTypeById);
+router.put("/:id", authMiddleware, ctrl.updateVacationType);
+router.delete("/:id", authMiddleware, ctrl.deleteVacationType);
 
 module.exports = router;
