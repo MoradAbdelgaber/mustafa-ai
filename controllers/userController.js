e36de66e4cd11ced08a69cc3602dee9a88a283d9
Morad Abdelgaber <moradabdelgaber1@gmail.com>
2025-02-15 19:21:41 +0200

    rm logs

--- controllers/userController.js
+++ controllers/userController.js
@@ -108,10 +108,7 @@ exports.getProfile = async (req, res) => {
 
 exports.updateProfile = async (req, res) => {
   try {
-    console.log(req.body);
     delete req.body.pass;
-    console.log(req.body);
-    console.log(req.user._id);
     const saved = await User.findByIdAndUpdate(req.user._id, req.body, {
       new: true,
     });
