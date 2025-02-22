56b9cbf15909d259b4e0b1eefcc26ad23882e6d0
Morad Abdelgaber <moradabdelgaber1@gmail.com>
2025-02-22 20:43:59 +0200

    testing

--- config/db.js
+++ config/db.js
@@ -3,13 +3,18 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    const dbPath =
+      process.env.TYPE == "DEV"
+        ? process.env.MONGO_URI_LOCAL
+        : process.env.MONGO_URI;
+
+    await mongoose.connect(dbPath, {
       useCreateIndex: true,
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
     });
-    console.log("MongoDB connected successfully.");
+    console.log("MongoDB connected successfully. : " + dbPath);
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
     process.exit(1);
--- server.js
+++ server.js
@@ -77,7 +77,8 @@ app.use(function (err, req, res, next) {
 });
 
 //start server
-const PORT = process.env.PORT || 3500;
+const PORT =
+  process.env.TYPE == "DEV" ? process.env.PORT_LOCAL : process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
