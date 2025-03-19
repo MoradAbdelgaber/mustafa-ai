8c2f3496e2a3c2291e55dbeb2cec0937d36b5f65
Morad Abdelgaber <moradabdelgaber1@gmail.com>
2025-03-19 22:46:00 +0200

    jsc

--- controllers/reportController1.js
+++ controllers/reportController1.js
@@ -1,2 +1,4 @@
-require('bytenode'); // تحميل bytenode لتفعيل قراءة ملفات .jsc
-const reportController = require('./reportController.jsc');
+require("bytenode"); // تحميل bytenode لتفعيل قراءة ملفات .jsc
+const reportController = require("./reportController.jsc");
+
+module.exports = reportController;
--- routes/reportRoutes.js
+++ routes/reportRoutes.js
@@ -3,24 +3,27 @@
 const express = require("express");
 const router = express.Router();
 const { authMiddleware, authEmployeeMiddleware } = require("../auth");
-const { getFullAttendanceReport } = require("../controllers/reportController");
-const { getDashboardMetrics } = require("../controllers/reportController");
-const {checkEmployeeStatus} = require("../controllers/reportController");
-const {getCustomTimeAttendanceReport} = require("../controllers/suddenlycheck");
+const { getFullAttendanceReport } = require("../controllers/reportController1");
+const { getDashboardMetrics } = require("../controllers/reportController1");
+const { checkEmployeeStatus } = require("../controllers/reportController1");
+const {
+  getCustomTimeAttendanceReport,
+} = require("../controllers/suddenlycheck");
 
 // مسار واحد للتقرير
 router.get("/attendance", authMiddleware, getFullAttendanceReport);
 
 //بصمة مفاجأة
-router.get("/getCustomTimeAttendanceReport", authMiddleware, getCustomTimeAttendanceReport);
+router.get(
+  "/getCustomTimeAttendanceReport",
+  authMiddleware,
+  getCustomTimeAttendanceReport
+);
 
 // تجييك موظف
 router.post("/checkEmployeeStatus", authMiddleware, checkEmployeeStatus);
 
-
-
-router.get("/dashboard-metrics", authMiddleware ,getDashboardMetrics);
-
+router.get("/dashboard-metrics", authMiddleware, getDashboardMetrics);
 
 //employee get his logs
 router.get(
